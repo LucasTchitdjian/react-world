4564c22d0779d411351ab88e726f912f8878e978
Lucas TCHITDJIAN <tchitdjian.lucas@gmail.com>
2022-02-11 16:19:45 +0100

    input-radio-finished

--- src/components/Contries.js
+++ src/components/Contries.js
@@ -7,6 +7,8 @@ const Contries = () => {
   const [sortedData, setSortedData] = useState([]);
   const [playOnce, setPlayOnce] = useState(true);
   const [rangeValue, setRangeValue] = useState(40);
+  const [selectedRadio, setSelectedRadio] = useState('');
+  const radios = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
 
   useEffect(() => {
     if (playOnce) {
@@ -32,11 +34,34 @@ const Contries = () => {
   return (
     <div className="contries">
     <div className="sort-container">
-      <input type="range" min="1" max="250" value={rangeValue} onChange={(e) => 
-        setRangeValue(e.target.value)}/>
+      <input
+        type="range" 
+        min="1" 
+        max="250" 
+        value={rangeValue}
+        onChange={(e) => setRangeValue(e.target.value)}
+      />
+      <ul>
+        {radios.map((radio) => {
+          return (
+            <li key={radio}>
+              <input type="radio" value={radio} id={radio}
+              checked={radio === selectedRadio} onChange={(e) => setSelectedRadio(e.target.value)} />
+              <label htmlFor={radio}>{radio}</label>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+    <div className="cancel">
+      {selectedRadio && (
+        <h5 onClick={() => setSelectedRadio("")}>Annuler recherche</h5>
+        )}
     </div>
       <ul className='contries-list'>
-        {sortedData.map((contry) => (
+        {sortedData
+          .filter((contry) => contry.region.includes(selectedRadio))
+          .map((contry) => (
           <Card contry={contry} key={contry.name.official} />
         ))}
       </ul>
