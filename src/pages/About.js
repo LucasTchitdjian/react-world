eb85496fff168d21f09c991298ffae57c5f910fb
Lucas TCHITDJIAN <tchitdjian.lucas@gmail.com>
2022-02-21 13:40:39 +0100

    End-of-routing

---
+++ src/pages/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>A propos</h1>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste voluptates,
+      nisi atque magnam possimus doloremque itaque tempore reiciendis ad labore 
+      perferendis aut quod ducimus! Maiores, fugiat sapiente quam quas beatae maxime veritatis 
+      molestias! Esse, incidunt. Ipsum corporis culpa sit unde maiores magnam eaque a at magni iusto, 
+      nam voluptas laudantium.</p>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste voluptates,
+      nisi atque magnam possimus doloremque itaque tempore reiciendis ad labore 
+      perferendis aut quod ducimus! Maiores, fugiat sapiente quam quas beatae maxime veritatis 
+      molestias! Esse, incidunt. Ipsum corporis culpa sit unde maiores magnam eaque a at magni iusto, 
+      nam voluptas laudantium.</p>
+    </div>
+  );
+};
+
+export default About;
