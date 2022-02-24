9906cafd9215c574b6fd9d54818088d7202ddd1a
Lucas TCHITDJIAN <tchitdjian.lucas@gmail.com>
2022-02-24 17:29:25 +0100

    date-format-and-latest-articles

--- src/pages/News.js
+++ src/pages/News.js
@@ -26,7 +26,9 @@ const News = () => {
         <input type="submit" value="Envoyer" />
       </form>
       <ul>
-        {newsData.map((article) => (
+        {newsData
+          .sort((a, b) => b.date - a.date)
+          .map((article) => (
           <Article key={article.id} article={article} />    
         ))}
       </ul>
